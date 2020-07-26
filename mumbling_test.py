1c2f6fed5b914ab2f05758ea55212e2351c1ee60
Joshua-Luke Bevan <joshua-luke@hotmail.co.uk>
2020-07-26 12:26:32 +0100

    Added test cases from problem

--- mumbling_test.py
+++ mumbling_test.py
@@ -13,6 +13,15 @@ class MumblingTestCase(unittest.TestCase):
     def test_mumble_single_string(self):
         self.assertEqual(mumble("a"), "A")
 
+    def test_mumbles_abc(self):
+        self.assertEqual(mumble("abC"), "A-Bb-Ccc")
+
+    def test_mumbles_abcd(self):
+        self.assertEqual(mumble("aBCd"), "A-Bb-Ccc-Dddd")
+
+    def test_mumbles_qwerty(self):
+        self.assertEqual(mumble("QWERTY"), "Q-Ww-Eee-Rrrr-Ttttt-Yyyyyy")
+
 
 if __name__ == '__main__':
     unittest.main()
